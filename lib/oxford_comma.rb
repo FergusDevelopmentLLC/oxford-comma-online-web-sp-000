4806b53915799c587027989c1df8f97844c991d5
Will Carter <will.carter@fergusllc.com>
2019-12-20 01:30:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
-  formatted = array
-  formatted[-1] = "and " + formatted.last
-  return array.join(", ")
+  if(array.length > 1)
+    formatted = array
+    formatted[-1] = "and " + formatted.last
+    return array.join(", ")
+  else
+    return array[0]
+  end
 end
 
 #puts oxford_comma(["fiddleheads","okra","kohlrabi"])
