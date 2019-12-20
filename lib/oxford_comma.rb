3fa70be0bcea2c823c2634538a784e980103254f
Will Carter <will.carter@fergusllc.com>
2019-12-20 01:30:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,6 @@ def oxford_comma(array)
   end
 end
 
-#puts oxford_comma(["fiddleheads","okra","kohlrabi"])
+puts oxford_comma(["fiddleheads","okra","kohlrabi"])
 #puts oxford_comma(["fiddleheads","okra"])
-puts oxford_comma(["fiddleheads"])
+#puts oxford_comma(["fiddleheads"])
